55fe1a1a8e0bc70a8becb06f62cac84236d5d77d
Francisco Blanco <blancofrancisco34@gmail.com>
2023-12-25 13:52:37 -0500

    flb

---
+++ src/components/Iframe/index.tsx
@@ -0,0 +1,19 @@
+export interface IframeProps {
+  src: string;
+}
+
+export const Iframe = ({ src }: IframeProps) => {
+  return (
+    <>
+      <iframe
+        src={src}
+        className="iframe"
+        style={{
+          width: "100%",
+          border: 0,
+          minHeight: "30dvh",
+        }}
+      />
+    </>
+  );
+};
