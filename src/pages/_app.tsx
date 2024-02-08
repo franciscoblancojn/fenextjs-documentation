ad4502074dc15ad220b237f5a6fd9d6f08426099
Francisco Blanco <blancofrancisco34@gmail.com>
2024-02-08 18:52:22 -0500

    favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,14 @@
+import Head from "next/head";
 import "../../styles/index.css";
 import { AppProps } from "next/app";
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>Fenextjs</title>
+        <link rel="icon" href="/favicon.png" sizes="32x32"></link>
+      </Head>
       <Component {...pageProps} />
     </>
   );
