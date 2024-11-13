be255146b3b4c5b684b4a61e711f31b0cadfaa1c
franciscoblancojn <blancofrancisco34@gmail.com>
2024-11-13 17:28:09 -0500

    fle

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,6 +10,12 @@ function CustomApp({ Component, pageProps }: AppProps) {
       <Head>
         <title>Fenextjs</title>
         <link rel="icon" href="/favicon.png" sizes="32x32"></link>
+        <meta property="og:title" content="Fenextjs" />
+        <meta
+          property="og:description"
+          content="Bienvenidos a Fenextjs, una librería innovadora diseñada para revolucionar tu experiencia de desarrollo en nextjs Nuestra misión es simplificar tu trabajo, ahorrándote tiempo valioso y energía en cada proyecto."
+        />
+        <meta property="og:image" content="/favicon.png" />
       </Head>
     </>
   );
